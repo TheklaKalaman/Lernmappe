1305d678aa888fc299d8328b370d47de3416a03b
Thekla Kalaman <thekla.kalaman@txs.de>
2025-08-04 23:07:39 +0200

    Geometrie: Kreis und Kreissegmente implementiert

--- CSharp/Geometrie/Program.cs
+++ CSharp/Geometrie/Program.cs
@@ -5,14 +5,26 @@ Console.WriteLine(A);
 var quadrat2 = new Quadrat(3);
 var Dreieck = new Dreieck(4, 5, 2, 4);
 var Rechteck = new Rechteck(3, 2);
+var Kreis = new Kreis(2);
+var Kreisausschnitt = new Kreisausschnitt(8, 170);
 
 Console.WriteLine(quadrat2.Flaecheninhalt);
 Console.WriteLine(Dreieck.Flaecheninhalt);
 Console.WriteLine(Rechteck.Flaecheninhalt);
+Console.WriteLine(Kreis.Flaecheninhalt);
+Console.WriteLine(Kreisausschnitt.Flaecheninhalt);
+
+Console.WriteLine(quadrat2.Flaecheninhalt
+        + Dreieck.Flaecheninhalt
+        + Rechteck.Flaecheninhalt
+        + Kreis.Flaecheninhalt
+        + Kreisausschnitt.Flaecheninhalt);
 
 var f = new KomplexeFigur();
 f.Hinzufuegen(new Quadrat(3));
 f.Hinzufuegen(new Dreieck(4, 5, 2, 4));
 f.Hinzufuegen(new Rechteck(3, 2));
+f.Hinzufuegen(new Kreis(2));
+f.Hinzufuegen(new Kreisausschnitt(8, 170));
 
 Console.WriteLine(f.Flaecheninhalt);
